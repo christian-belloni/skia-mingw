834cc170a237722c76510f723a051d3331163c0a
unknown <christian.belloni@babyloncloud.com>
2026-07-10 13:00:40 +0200

    fixed prepare_src.sh

--- prepare_srcs.sh
+++ prepare_srcs.sh
@@ -10,20 +10,10 @@ apply_patch_with_msg() {
 tar xvf skia.tar.gz
 rm skia.tar.gz
 
-mv skia-da51f0d60ea2b14e845a823dc11b405dbeef42d8 skia
-
 cd skia
 
 apply_patch_with_msg \
-    0001-add-pkgconfig-support.patch \
-    0002-add-mingw-toolchain-skia.patch \
-    0003-fix-dwrite-function-mingw.patch \
-    0004-misc-mingw-fixes.patch \
-    0005-external-packages-update.patch \
-    0006-integrates-svg-into-skia-lib.patch \
-    0008-dwrite.patch \
-    0009-allow-opengl-aarch64.patch \
-    0010-support-vulkan.patch
+    0001-support-freya.patch
 
 patch -R -Nbp1 -i "${srcdir}/0007-Reland-Make-SkPath-immutable-on-GN-build.patch"
 
